dc3870aab9b042a98095fa904d82b8e4d1e85d10
Dishant-dyavarchetti <ddishantmsccs2023@gmail.com>
2026-03-21 14:24:44 +0530

    created and setup the docker compose and done connectivity of backend and mysql container

--- learnova/learnova/settings.py
+++ learnova/learnova/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'learnova.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learnova',
+        'USER': 'admin',
+        'PASSWORD': 'qwerty',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
